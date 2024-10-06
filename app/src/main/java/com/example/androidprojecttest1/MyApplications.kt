8cc49d7c1f821ec86b672621065f0b8205606b5b
AsefQurbanov <qurbanovasef1998@gmail.com>
2024-10-06 15:26:15 +0400

    Added log to application class onCreate

--- app/src/main/java/com/example/androidprojecttest1/MyApplications.kt
+++ app/src/main/java/com/example/androidprojecttest1/MyApplications.kt
@@ -1,6 +1,7 @@
 package com.example.androidprojecttest1
 
 import android.app.Application
+import android.util.Log
 import com.example.androidprojecttest1.lesson25.Database
 import dagger.hilt.android.HiltAndroidApp
 
@@ -10,5 +11,7 @@ class MyApplications : Application() {
         super.onCreate()
 
         Database.initDatabase(this)
+
+        Log.d("AAA", "Application: onCreate")
     }
 }
